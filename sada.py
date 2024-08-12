e4aedf48163640dbf376f74480bbdc353728a7ac
Mr.Chen <379337152@qq.com>
2024-08-12 16:54:15 +0800

    提交消息

--- sada.py
+++ sada.py
@@ -1,16 +1,17 @@
-import subprocess
-import os
-# 仓库路径
+from git import Repo
+
+# 本地仓库路径
 repo_path = 'L:/Mr.Chen 设计环境/影视相关/ipv4'
 
-# 切换到仓库目录
-os.chdir(repo_path)
+# 打开仓库
+repo = Repo(repo_path)
 
-# 添加文件
-subprocess.run(["git", "add", "--all"])
+# 添加所有更改的文件
+repo.git.add('--all')
 
 # 提交更改
-subprocess.run(["git", "commit", "-m", "提交消息"])
+repo.index.commit("提交消息")
 
 # 推送到远程仓库
-subprocess.run(["git", "push", "origin", "main"])
+origin = repo.remote(name='origin')
+origin.push()
